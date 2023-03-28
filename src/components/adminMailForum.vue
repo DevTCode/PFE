7f69c5120ab3c4db5b5d3015345b9a5aebc20962
DevTCode <assimichraq@gmail.com>
2023-03-28 17:47:45 +0200

    Update adminMailForum.vue

--- src/components/adminMailForum.vue
+++ src/components/adminMailForum.vue
@@ -1,37 +1,17 @@
 
 <template>
-  <v-app>
-   <v-app-bar style="padding: 0px 90px"
- app color="#243763" elevation="6" >
-     
-      <v-toolbar-title >
-       <v-img
-          :src="require('../assets/im.jpeg')"
-          
-          contain
-          height="200"
-          width="150"
-        />
-     </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-list-item link v-for="(menu,index) in menus" :key="index" :to="menu.route">
-      <v-list-item-title style="color:white">{{menu.title}}</v-list-item-title>
-      </v-list-item>
-    </v-app-bar>
-
-<v-container class="my-15">
-<v-row justify="center">
-<v-col lg="12" class="text-center">
-
-</v-col>
-<v-col lg="6" >
-<img src="https://img.freepik.com/vecteurs-premium/concept-partage-voiture_118813-3882.jpg?w=1060" class="im">
-</v-col>
-<v-col lg="6">
-
-<v-sheet width="450" height="400" style="opacity:90%" class="mx-auto" >
-<br><br><br><br><br><br>
-<v-form @submit.prevent="submit">
+  <body>
+    <section class="form">
+    <div class="container">
+    <div class="row no-gutters">
+    <div class="col-lg-5">
+    <v-img :src="require('../assets/car.jpg')" contain class="nj"></v-img>
+    </div>
+    <div class="col-lg-7">
+    <v-img
+          :src="require('../assets/lgo.png')" contain height="30%" width="26%" class="n"></v-img>
+          <h4>Sign into your account</h4>
+    <v-form @submit.prevent="submit" >
   
   
       <v-col cols="auto">
@@ -40,40 +20,82 @@
       
    
     
-    <v-text-field v-model="email" label="email"></v-text-field>
-    <v-text-field v-model="password" label="Mot de passe" type="password"></v-text-field>
+    <v-text-field v-model="email" label="email" class="e"></v-text-field>
+    <v-text-field v-model="password" label="Mot de passe" type="password" class="psw"></v-text-field>
     
-    <v-btn class="text-center" rounded="pill" color="#243763" size="large" @click="login"> <b class="b">Login</b></v-btn>
+    <v-btn class="btn1 mt-3 mb-5" @click="login"> <b class="b">Login</b></v-btn>
     
   </v-form>
-</v-sheet>
-
-</v-col>
-</v-row>
-</v-container>
-  </v-app>
+    </div>
+    </div>
+    </div>
+    </section>
   
+  </body>  
 </template>
 
 
 
 <style>
-.im{
-    opacity: 70%;
-}
-.my-custom-class{
-    color:#FF6E31 ;
-}
-.v-toolbar-title{
-     font-size: 25px;
-     color: #FF6E31;
-     font-weight: 700;
-     font-family: Arial, Helvetica, sans-serif;
- }
+
  .b{
      color: white;
  }
- 
+ .row{
+   background: black;
+   border-radius:20px ;
+   width:95%;
+   padding: 2%;
+   box-shadow: 12px 12px 22px #950101;
+   
+ }
+
+ *{
+   padding: 0;
+   margin:0;
+   box-sizing: border-box;
+ }
+
+ .nj{
+   width: 90%;
+   height: 120%;
+   border-top-left-radius: 30px;
+   border-bottom-left-radius: 30px;
+   
+ }
+ .e{
+   color: white;
+   background-color: transparent;
+ }
+ .psw{
+   color: white;
+ }
+ .btn1{
+   border: none;
+   outline: none;
+   height: 50px;
+   width: 100%;
+   background-color: #950101;
+   border-radius: 4px;
+   font-size:15px ;
+   font-family: cursive;
+ }
+ .btn1:hover{
+   background: black;
+   border: 1px solid;
+   color: #950101;
+ }
+ h4{
+   color: #3E3636;
+   padding: 10px;
+   font-family: Arial, Helvetica, sans-serif;
+ }
+ h2{
+   color: white;
+   font-size: 100px;
+   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+   
+ }
 </style>
 
 
