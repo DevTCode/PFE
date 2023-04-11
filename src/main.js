157741fe7c815e69a4df49dd2b796b46e7147e23
DevTCode <assimichraq@gmail.com>
2023-04-11 18:11:20 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import LaravelVuePagination from 'laravel-vue-pagination'
+import JwPagination from 'jw-vue-pagination';
 import "bootstrap-icons/font/bootstrap-icons.css"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 import { loadFonts } from './plugins/webfontloader'
 
+export default {
+  components: {
+    'Pagination' : LaravelVuePagination
+  }
+}
 loadFonts()
 
 createApp(App)
@@ -14,3 +22,5 @@ createApp(App)
   .use(vuetify)
   .use(bootstrap)
   .mount('#app')
+  
+  
