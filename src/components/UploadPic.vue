0bfd2d3fe3d3061e10e9f4b16eb1dac72d06604b
DevTCode <assimichraq@gmail.com>
2023-04-11 17:44:20 +0200

    Create UploadPic.vue

---
+++ src/components/UploadPic.vue
@@ -0,0 +1,195 @@
+<template>
+
+
+  <body id="f">
+ <br><br><br>
+    <section class="form">
+    <div class="container">
+    <div class="row no-gutters">
+    <div class="col-lg-5"  style="height:560px">
+    <v-img :src="require('../assets/h.jpg')" contain class="h"></v-img>
+    </div>
+    <div class="col-lg-7">
+    <v-img
+          :src="require('../assets/lgo.png')" contain height="30%" width="26%" class="n"></v-img>
+          
+    <v-form @submit.prevent="submit" >
+  
+  
+      <v-col cols="auto">
+        <h2>Image</h2>
+      </v-col>
+      
+   
+    
+    
+         <div class="container">
+        <form @submit.prevent="submit">
+        <v-file-input label="File input" v-on:change="onChange" @change="previewImage" variant="filled" prepend-icon="mdi-camera" style="color:white"></v-file-input>
+        
+        <br><br>
+      <img :src="imageUrl" v-if="imageUrl" style="width:200px">
+         </form>
+       </div>
+   <br>
+    
+    <v-btn class="btn1 mt-3 mb-5"> <b class="b">Add</b></v-btn>
+    
+    </v-form>
+    </div>
+    </div>
+    </div>
+  
+    </section>
+  
+  </body>  
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: "UploadPic.vue",
+    data(){
+        return{
+         imageUrl: '',
+    }
+
+},
+methods: {
+    previewImage(event) {
+      const file = event.target.files[0];
+      this.imageUrl = URL.createObjectURL(file);
+    },
+      onChange:function(e){
+         var images =  e.target.files[0];
+         console.log("selected file", images)
+         var fd = new FormData();
+         fd.append('image', images);
+         axios.post('http://localhost:8000/api/upload', fd).then(res=>{
+         console.log(res.data)
+         this.image=res.data;
+      })
+     },
+    }
+}
+</script>
+
+
+<style scoped>
+:rout {
+    --main-bg-color: black;
+    --main-text-color: #950101;
+    --second-text-color: white;
+    --second-bg-color: grey;
+}
+.primary-text{
+    color: var(--main-text-color);
+}
+.second-text{
+    color: var(--second-text-color);
+}
+.primary-bg{
+    background-color: var(--main-bg-color);
+}
+
+.secondry-bg{
+    background-color: var(--second-bg-color);
+}
+.rounded-full{
+    border-radius: 100%;
+}
+#wrapper{
+    overflow-x: hidden;
+   background-color: white;
+   height: 812px;
+}
+#sidebar-wrapper{
+    min-height: 100vh;
+    margin-left: -15rem;
+    transition: margin 0.25s ease-out;
+}
+#sidebar-wrapper .sidebar-heading{
+    padding: 0.875rem 1.25rem;
+    font-size: 1.2rem;
+}
+#sidebar-wrapper .list-group{
+    width:15rem;
+}
+#page-content-wrapper{
+   
+  background-size: cover;
+     background-image: url('https://wallpapercave.com/wp/wp6071422.jpg');
+}
+
+#wrapper.toggled #sidebar-wrapper{
+    margin-left: 0;
+}
+#menu-toggle{
+    cursor: pointer;
+}
+.list-group-item{
+    border: none;
+    padding: 20px 30px;
+    width: 100%;
+    color: black;
+    background-color: white;
+
+}
+#navbarDropdown{
+    font-size: 18px;
+    color: white;
+}
+.list-group-item.active{
+    background-color: transparent;
+    color:var(--main-text-color);
+    font-weight: bold;
+    border: none;
+}
+.cr{
+    color: #950101;
+    text-align: center;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-size: 40px;
+    
+}
+ .main-btn{
+     display: inline-block;
+     padding: 0.625rem 1.875rem;
+     line-height: 1rem;
+     background-color: white;
+    
+     color: black;
+     font-size: 20px;
+     font-weight: 600;
+     text-transform: capitalize;
+     border-radius: 2rem;
+     box-shadow: 0px 2px 10px -1px rgb(0 0 0/ 19%);
+     -webkit-transition: all .4s ease-out 0s;
+     -o-transition: all .4s ease-out 0s;
+     -moz-transition: all .4s ease-out 0s;
+     transition: all .4s ease-out 0s;
+     
+ }
+ .main-btn hover{
+     background-color: transparent;
+     color: black;
+ }
+@media(min-width:768px){
+    #sidebar-wrapper{
+        margin-left: 0;
+    }
+    #page-content-wrapper{
+        min-width: 0;
+        width: 100%;
+      
+    }
+    #wrapper.toggled #sidebar-wrapper{
+        margin-left: -15rem;
+    }
+    .custom-link{
+        color: black;
+    }
+}
+</style>
+
+
