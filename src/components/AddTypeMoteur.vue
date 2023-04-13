882dc46f8c8b685adbcacf6056313e1107ee5d83
DevTCode <assimichraq@gmail.com>
2023-04-13 03:51:58 +0200

    Update AddTypeMoteur.vue

--- src/components/AddTypeMoteur.vue
+++ src/components/AddTypeMoteur.vue
@@ -4,7 +4,7 @@
  <br><br>
     <section class="form">
     <div class="container">
-    <div class="row no-gutters">
+    <div class="row no-gutters" style="width:1300px">
     <div class="col-lg-5">
     <v-img :src="require('../assets/bck.jpg')" contain class="z"></v-img>
     </div>
@@ -16,7 +16,7 @@
   
   
       <v-col cols="auto">
-        <h2>Type de moteur</h2>
+        <h4 style="color:white;font-weight:600;font-size:30px">Type de moteur</h4>
       </v-col>
       
    
@@ -24,7 +24,7 @@
     <v-text-field v-model="libelle" label="libelle" class="e"></v-text-field>
    
     
-    <v-btn class="btn1 mt-3 mb-5" @click="saveTypeMoteur"> <b class="b">Add</b></v-btn>
+    <v-btn class="btn1 mt-3 mb-5" @click="saveTypeMoteur"> <b class="b">Ajouter</b></v-btn>
     
   </v-form>
     </div>
@@ -122,6 +122,4 @@ export default {
 
 
 </script>
-
-
 
